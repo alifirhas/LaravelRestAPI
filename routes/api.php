f13ea9e121af00dcdea8b9142d8a08073e18b6ff
alifirhas <irhasalif@gmail.com>
2021-05-08 11:58:12 +0700

    database setup, api get all post data, and api create post

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,8 +13,28 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+//Route untuk menampilkan semua post
+Route::get('/posts', function () {
+    return Post::all();
+});
+
+//Route untuk menambahkan post
+Route::post('/posts', function () {
+
+    request()->validate([
+        'title' => 'required|max:254',
+        'content' => 'required',
+    ]);
+
+    return Post::create([
+        'title' => request('title'),
+        'content' => request('content'),
+    ]);
+
 });
